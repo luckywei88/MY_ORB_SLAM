643a82b9a416af13ec6711d0adc77f68e6da7463
luckywei88 <1010224593@qq.com>
2018-01-24 01:00:29 +0800

    upload

--- src/DATASET/rgbd_tum.cc
+++ src/DATASET/rgbd_tum.cc
@@ -118,8 +118,7 @@ int main(int argc, char **argv)
             T = tframe-vTimestamps[ni-1];
 
         if(ttrack<T)
-            usleep((T)*1e6*15);
-            //usleep((T-ttrack)*1e6*50);
+            usleep((T-ttrack)*1e6);
     }
 
     // Stop all threads
